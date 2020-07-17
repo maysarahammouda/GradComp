89facaf14c80b41f24a4629cb02862e1c20484dd
maysarahammouda <maysara.fayez@gmail.com>
2020-07-17 11:19:30 +0100

    few updates ..

--- compressor/variance_based - Copy.py
+++ compressor/variance_based - Copy.py
@@ -39,7 +39,7 @@ class VarianceBasedCompressor(Compressor):
             compression_ratio: the amount of compression we get after compressing
                                 the gradients.
         """
-        tensors = sparsify3(tensor, self.alpha, self.batch_size)
+        tensors = sparsify(tensor, self.alpha, self.batch_size)
         ctx = tensor.numel(), tensor.size()
 
         self.total_origional += tensor.numel()
@@ -78,37 +78,6 @@ def sparsify(tensor, alpha, batch_size):
     """
     tensor = tensor.flatten()
     r,v = 0,0
-    val,ind = [],[]
-    gamma = 0.999
-
-    for i in range(len(tensor)):
-        r += tensor[i]/batch_size
-        v += pow(tensor[i]/batch_size,2)
-        if pow(r,2) > alpha*v:
-            val.append(tensor[i])
-            ind.append(i)
-            r = 0
-            v = 0
-        else:
-            v *= gamma
-
-    values = torch.tensor(val)
-    indices = torch.tensor(ind)
-    return values, indices
-
-
-def sparsify2(tensor, alpha, batch_size):
-    """
-    This function performs "sparsification" for "tensor".
-    It decides on the number of elements to keep based on the "compress_ratio".
-    Args:
-        tensor: the tensor we need to sparsify.
-        compress_ratio: the percentage of the number of elements we want to keep.
-    Return:
-        the values and indices for the choosen elements.
-    """
-    tensor = tensor.flatten()
-    r,v = 0,0
     ind = torch.zeros(tensor.numel())
     gamma = 0.999
 
@@ -125,86 +94,6 @@ def sparsify2(tensor, alpha, batch_size):
     indices = torch.where(ind>0)[0].flatten()
     values = tensor[indices]
     return values, indices
-
-
-def sparsify4(tensor, alpha, batch_size):
-    """
-    This function performs "sparsification" for "tensor".
-    It decides on the number of elements to keep based on the "compress_ratio".
-    Args:
-        tensor: the tensor we need to sparsify.
-        compress_ratio: the percentage of the number of elements we want to keep.
-    Return:
-        the values and indices for the choosen elements.
-    """
-    tensor = tensor.flatten()
-    r,v,m = 0,0,1
-    ind = torch.zeros(tensor.numel())
-    gamma = 0.999
-
-    r = torch.cumsum(tensor/batch_size, dim=0)
-    v = torch.cumsum(pow(tensor/batch_size,2), dim=0)
-
-    for i in range(len(tensor)):
-        if pow(r[m-1],2) > alpha*v[m-1] or m == len(r) -1:
-            ind[i] = 1
-            r = torch.cumsum(tensor[i:i+100]/batch_size, dim=0) if len(tensor[i:]>100) else torch.cumsum(tensor[i:]/batch_size, dim=0)
-            v = torch.cumsum(pow(tensor[i:i+100]/batch_size,2), dim=0) if len(tensor[i:]>100) else torch.cumsum(tensor[i:]/batch_size, dim=0)
-            m = 1
-        else:
-            v[m-1] *= gamma
-            m += 1
-
-    indices = torch.where(ind>0)[0].flatten()
-    values = tensor[indices]
-    return values, indices
-
-
-def sparsify3(tensor, alpha, batch_size):
-    """
-    This function performs "sparsification" for "tensor".
-    It decides on the number of elements to keep based on the "compress_ratio".
-    Args:
-        tensor: the tensor we need to sparsify.
-        compress_ratio: the percentage of the number of elements we want to keep.
-    Return:
-        the values and indices for the choosen elements.
-    """
-    tensor = tensor.flatten()
-    r,v,m = 0,0,1
-    ind = torch.zeros(tensor.numel())
-    gamma = 0.999
-
-    r = torch.cumsum(tensor/batch_size, dim=0)
-    v = torch.cumsum(pow(tensor/batch_size,2), dim=0)
-
-    for i in range(len(tensor)):
-        if pow(r[m-1],2) > alpha*v[m-1]:
-            ind[i] = 1
-            r = torch.cumsum(tensor[i:]/batch_size, dim=0)
-            v = torch.cumsum(pow(tensor[i:]/batch_size,2), dim=0)
-            m = 0
-        else:
-            v[m-1] *= gamma
-            m += 1
-
-    indices = torch.where(ind>0)[0].flatten()
-    values = tensor[indices]
-    return values, indices
-#
-# for rr in r:
-#     for vv in v:
-#         if pow(rr,2) > alpha*vv:
-#             # print("i inside",i)
-#             # print("i-m:", i-m)
-#             ind[0] = 1
-#             r = torch.cumsum(tensor[i:]/batch_size, dim=0)
-#             # print("length of r", len(r))
-#             v = torch.cumsum(pow(tensor[i:]/batch_size,2), dim=0)
-#             # print("r", r)
-#             # print("v", v)
-#             # print(ind)
-#             # m = i
 
 
 def desparsify(tensors, numel):
